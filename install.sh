c5ac0b72ef40ee544219ca1d0ef9a5f0eb57ed7b
Luca Ionescu <lucaioanionescu@gmail.com>
2018-08-31 18:50:13 +0200

    added pklist and python requirements.txt. updated todo list:

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 # dotfiles installation dir 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# install packages
+pacman -S --needed < "$DOTFILES_DIR/pkglist"
+
+# install python packages
+pip3 install -r "$DOTFILES_DIR/requirements.txt"
+
+# create symbolic links
 ln -sf "$DOTFILES_DIR/bash/.bashrc" ~
 ln -sf "$DOTFILES_DIR/bash/.bash_aliases" ~
 ln -sf "$DOTFILES_DIR/bash/.bash_functions" ~
@@ -11,6 +18,8 @@ ln -sf "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sf "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sf "$DOTFILES_DIR/pureline/.pureline.conf" ~
 ln -sf "$DOTFILES_DIR/i3/config" ~/.i3/
+ln -sf "$DOTFILES_DIR/code/settings.json" ~/.config/Code/User/
+ln -sf "$DOTFILES_DIR/code/keybindings.json" ~/.config/Code/User/
 
 # install pureline
 git clone https://github.com/chris-marsh/pureline.git ~/pureline/
@@ -26,3 +35,4 @@ code --install-extension PeterJausovec.vscode-docker
 code --install-extension ritwickdey.LiveServer
 code --install-extension robertohuertasm.vscode-icons
 code --install-extension tomoki1207.pdf
+
--- todo.md
+++ todo.md
@@ -5,5 +5,4 @@
 - Dive into emacs
 - Write script for (un-)connecting external monitor
 - Write function for opening man pages as pdf
-- Add Python requirements
-- Add package list for pacman
+
