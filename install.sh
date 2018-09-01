593c2e4dee8d1633e1a57038285a4f7d56f84d22
Luca Ionescu <lucaioanionescu@gmail.com>
2018-09-01 13:29:11 +0200

    install.sh: rearranged.

--- install.sh
+++ install.sh
@@ -9,18 +9,6 @@ pacman -S --needed < "$DOTFILES_DIR/pkglist"
 # install python packages
 pip3 install -r "$DOTFILES_DIR/requirements.txt"
 
-# create symbolic links
-ln -sf "$DOTFILES_DIR/bash/.bashrc" ~
-ln -sf "$DOTFILES_DIR/bash/.bash_aliases" ~
-ln -sf "$DOTFILES_DIR/bash/.bash_functions" ~
-ln -sf "$DOTFILES_DIR/bash/.bash_profile" ~
-ln -sf "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sf "$DOTFILES_DIR/git/.gitignore_global" ~
-ln -sf "$DOTFILES_DIR/pureline/.pureline.conf" ~
-ln -sf "$DOTFILES_DIR/i3/config" ~/.i3/
-ln -sf "$DOTFILES_DIR/code/settings.json" ~/.config/Code/User/
-ln -sf "$DOTFILES_DIR/code/keybindings.json" ~/.config/Code/User/
-
 # install pureline
 git clone https://github.com/chris-marsh/pureline.git ~/pureline/
 
@@ -36,3 +24,14 @@ code --install-extension ritwickdey.LiveServer
 code --install-extension robertohuertasm.vscode-icons
 code --install-extension tomoki1207.pdf
 
+# create symbolic links
+ln -sf "$DOTFILES_DIR/bash/.bashrc" ~
+ln -sf "$DOTFILES_DIR/bash/.bash_aliases" ~
+ln -sf "$DOTFILES_DIR/bash/.bash_functions" ~
+ln -sf "$DOTFILES_DIR/bash/.bash_profile" ~
+ln -sf "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sf "$DOTFILES_DIR/git/.gitignore_global" ~
+ln -sf "$DOTFILES_DIR/pureline/.pureline.conf" ~
+ln -sf "$DOTFILES_DIR/i3/config" ~/.i3/
+ln -sf "$DOTFILES_DIR/code/settings.json" ~/.config/Code/User/
+ln -sf "$DOTFILES_DIR/code/keybindings.json" ~/.config/Code/User/
