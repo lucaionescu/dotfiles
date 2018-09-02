f8a565d829d1202b77d37f61af742d9c58f91e34
Luca Ionescu <lucaioanionescu@gmail.com>
2018-09-02 11:52:32 +0200

    emacs themes folder.

--- emacs/init.el
+++ emacs/init.el
@@ -1,33 +1,27 @@
-; show line numbers
-; (global-linum-mode)
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(add-hook 'prog-mode-hook 'linum-mode)
+;; use y or n instead of yes or no
+(fset 'yes-or-no-p 'y-or-n-p)
 
-; padding between line number and text
-; (setq linum-format "%d ")
+;; show line numbers
+(global-linum-mode)
+
+;; do not create annoying backup files
+(setq make-backup-files nil)
+
+;; highlight matching paranthesis
+(show-paren-mode 1)
 
 ;; julia mode
 (add-to-list 'load-path "~/.emacs.d/modes/")
-(require 'julia-mode)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes (quote (manoj-dark))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+  (require 'julia-mode)
+
+;; themes
+(add-to-list 'load-path "~/.emacs.d/themes/")
+
+(load-file "~/.emacs.d/themes/klere-theme.el")
+
