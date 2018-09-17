1b55a34d8db83d75d75067dbe4686e820bd7e764
Luca Ionescu <lucaioanionescu@gmail.com>
2018-09-17 23:39:09 +0200

    updates.

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ pip3 install -r "$DOTFILES_DIR/requirements.txt"
 git clone https://github.com/chris-marsh/pureline.git ~/pureline/
 
 # install vscode extensions
-code --install-extension Equinusocio.vsc-material-theme
 code --install-extension julialang.language-julia
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
@@ -21,8 +20,8 @@ code --install-extension ms-vscode.Go
 code --install-extension naumovs.color-highlight
 code --install-extension PeterJausovec.vscode-docker
 code --install-extension ritwickdey.LiveServer
-code --install-extension robertohuertasm.vscode-icons
 code --install-extension tomoki1207.pdf
+code --install-extension equinusocio.vsc-material-theme
 
 # create symbolic links
 ln -sf "$DOTFILES_DIR/bash/.bashrc" ~
