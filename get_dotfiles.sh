2939c1975b5415281c38bfbea5fbcf8f3b81b30e
Luca Ionescu <lucaionescu@users.noreply.github.com>
2019-06-10 19:54:49 +0200

    added script for downloading dotfiles.

---
+++ get_dotfiles.sh
@@ -0,0 +1,33 @@
+    # dotfiles repository download script
+    # Originally written by Darryl Abbate (https://github.com/rootbeersoup)
+    # curl -fsSL https://lucaionescu.github.io/dotfiles.sh | sh
+
+    dotfiles_check ()
+    {
+      if [ -d "$HOME/dotfiles" ]; then
+        printf "~/dotfiles directory already exists.\\n" &gt;&amp;2
+        exit 1
+      fi
+    }
+
+    main()
+    {
+      if ! [ -x "$(command -v git)" ]; then
+      curl -L https://github.com/lucaionescu/dotfiles/archive/master.tar.gz -o "$HOME/dotfiles-master.tar.gz"
+      tar xzf "$HOME/dotfiles-master.tar.gz"
+      mv "$HOME/dotfiles-master" "$HOME/dotfiles"
+
+      else
+        git clone https://github.com/lucaionescu/dotfiles.git "$HOME/dotfiles"
+      fi
+
+      if [ -d "$HOME/dotfiles" ]; then
+        printf "repository downloaded to ~/dotfiles\\n\\n"
+      else
+        printf "dotfiles either not downloaded or not extracted successfully\\n" &gt;&amp;2
+        exit 1
+      fi
+    }
+
+    dotfiles_check
+    main
