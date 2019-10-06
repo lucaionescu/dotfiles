30049f71f0b77072f9fce0bfe716a7c85508ef99
Luca Ionescu <lucaionescu@users.noreply.github.com>
2019-10-06 11:54:25 +0200

    add vscode config.

---
+++ vscode/install_extensions.sh
@@ -0,0 +1,8 @@
+code --install-extension coenraads.bracket-pair-colorizer-2
+code --install-extension naumovs.color-highlight
+code --install-extension ms-vscode.go
+code --install-extension torn4dom4n.latex-support
+code --install-extension ritwickdey.liveserver
+code --install-extension acidic9.p5js-snippets
+code --install-extension ms-python.python
+code --install-extension vscode-icons-team.vscode-icons
